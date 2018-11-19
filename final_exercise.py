0e8125905da328e7b7284c1c9b8a464b7f0fdb71
Anuj Menta <anujmenta@gmail.com>
2018-11-20 00:15:20 +0530

    Includes Neural networks

--- final_exercise.py
+++ final_exercise.py
@@ -1,1 +1,15 @@
-
+import pandas
+import requests
+from time import time
+d= pd.read_csv('lbv.csv')
+def dateclean( row):
+    date,time =row['CHARTTIME'].split('')
+    return pandas.Series([date,time)
+d[['time_hour','time_min','time_sec']] = d.apply(dateclean,axis=1)
+final_df = df[[' UREA_VALUE','Potassium_Level','WBC_VALUE','Sodium_Level','B_VALUE','DIE_IN_24HRS','time_hour','time_min','time_sec']]
+net = tflearn.input_data(shape=[None,8])
+net = tflearn.fully_connected(net,24)
+net = tflearn.fully_connected(net,2,activation='softmax')
+model = tflearn.DNN(net)
+model.fit(data,labels,n_epoch=5,batch_size=100,show_metric=True)
+model.save('initial100iter_1.tflearn')
